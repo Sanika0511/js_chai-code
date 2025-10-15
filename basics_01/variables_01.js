0720ff9e9720b797c7e65266e8712caaa85c25f5
Sanika0511 <mailtosangaikwad@gmail.com>
2025-10-15 21:13:12 +0000

    variables learnt

---
+++ basics_01/variables_01.js
@@ -0,0 +1,24 @@
+const accountId=431001
+let accountEmail="sanika@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur" //should not be used
+let accouontState; //undefined
+
+// accountId=2// not allowed
+
+// {
+// scope
+// }
+
+/*
+Prefer not to use var
+because of issue in block scope and fuctional scope
+*/
+
+accountEmail="san@gmail.com"
+accountPassword="324663"
+accountCity="Lucknow"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accouontState])
