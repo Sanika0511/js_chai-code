555c34ca2397f023c17147adaf1fca99aded2502
Sanika0511 <mailtosangaikwad@gmail.com>
2025-10-15 22:00:06 +0000

    operations learnt

---
+++ basics_01/conversionOperations_03.js
@@ -0,0 +1,57 @@
+// let score = "Sanika"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// // "33" => 33
+// // "33abc" => Nan
+// // true => 1: false =>0
+
+// let isLoggedIn="Sanika"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// // 1 => true; 0 => false
+// // "" => false
+// // "Sanika => true"
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******** Operations *********
+
+let value = 3
+let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 =" Sanika"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+4*5%3); //avoid this type of writing
+
+console.log(+true);
+// console.log(true+);
+console.log(+"");
