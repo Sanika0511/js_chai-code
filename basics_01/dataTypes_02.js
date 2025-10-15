92873a509b903a8d996f110cbdcff618a599a2e6
Sanika0511 <mailtosangaikwad@gmail.com>
2025-10-15 21:22:41 +0000

    data types learnt

---
+++ basics_01/dataTypes_02.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we r using node.js, not browser
+
+console.log(3
+    +         //code readability should be high
+    3)
+
+/* ecma */
+console.log("Sanika");
+
+
+let name="Sanika"
+let age=20
+let isLoggedIn=false
+
+//number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not assigned values yet
+//symbol => to define uniqueness
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
