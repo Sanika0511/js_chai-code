3e352e6613e105e2b1cde7405f34afa1c12e66d9
Sanika0511 <mailtosangaikwad@gmail.com>
2025-10-19 17:24:56 +0000

    datatypes summary done

---
+++ basics_01/datatypes_summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types ; String, Number, Boolean, Null, Undefined, Symnol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 845629449841846294894623184956154n //bigint
+
+// console.log(typeof bigNumber);
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Sanika",
+    age : 20
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+// console.log(typeof myFunction);
+console.log(typeof outsideTemp);
