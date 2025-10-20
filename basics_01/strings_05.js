3dbe218276eb978bc4608bcc745547d0ec42fc6f
Sanika0511 <mailtosangaikwad@gmail.com>
2025-10-20 19:04:15 +0000

    strings done

---
+++ basics_01/strings_05.js
@@ -0,0 +1,38 @@
+const name = "sanika"
+const repoCount = 50
+
+console.log(name  + repoCount + "Value");
+
+console.log(`My name is ${name} and repo count is ${repoCount}`);
+
+const gameName = new String('sanika-sg')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('i'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+ 
+const newStringOne = "     sanika    "
+console.log(newStringOne);
+console.log(newStringOne.trim( ));
+
+
+const url = "https://sanika.com/sanika%20gaikwad"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sanika'));
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+
