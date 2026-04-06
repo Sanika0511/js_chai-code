bfc1d5c9b1f07061435f2da5050f32c3901238cb
Sanika0511 <mailtosangaikwad@gmail.com>
2026-04-06 20:02:50 +0000

    more changes

--- basics_01/conversionOperations_03.js
+++ basics_01/conversionOperations_03.js
@@ -45,13 +45,13 @@ let negValue= -value
 // let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1"+2);
-// console.log(1+"2");
-// console.log("1"+2+2);
-// console.log(1+2+"2");
+// console.log("1"+2); => 12
+// console.log(1+"2"); => 12
+// console.log("1"+2+2); => 122
+// console.log(1+2+"2"); => 32
 
 // console.log(3+4*5%3); //avoid this type of writing
 
-console.log(+true);
-// console.log(true+);
-console.log(+"");
+// console.log(+true); => 1
+// console.log(true+); => error
+// console.log(+""); => 0
